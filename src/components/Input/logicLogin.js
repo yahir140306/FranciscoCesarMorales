f4fa4ef54fc3f6ff5e30d25d78287f7aed9d71d7
yahir140306 <joseyahirhernandez11@gmail.com>
2024-05-12 22:35:09 -0600

    Desarrollando la pagina de Entrar y Crear

---
+++ src/components/Input/logicLogin.js
@@ -0,0 +1,26 @@
+const INPUT = [
+    {
+        span: "Nombre del alumno: ",
+        type:"text",
+    },
+    {
+        span: "Nombre del Padre/Tutor: ",
+        type:"text",
+    },
+    {
+        span: "CURP: ",
+        type:"text",
+    },
+    {
+        span: "Correo Electronico: ",
+        type:"email",
+    },
+    {
+        span: "Contraseña: ",
+        type:"email",
+    },
+    {
+        span: "Confirmar Contraseña: ",
+        type:"email",
+    }
+]
