5fb8cae9e174ac4ea3646fecf2716162696180f6
yahir140306 <joseyahirhernandez11@gmail.com>
2024-06-11 23:33:02 -0600

    Add new components POST

---
+++ src/components/Newtworks/ContentPosto.js
@@ -0,0 +1,38 @@
+export const POSTS = [
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Ab aliquid mollitia pariatur natus eaque omnis obcaecati corrupti, accusantium quibusdam ea.",
+        image: "/Images/Carrusel/Image7.jpg",
+        descripcion: "imagen"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Ab aliquid mollitia pariatur natus eaque omnis obcaecati corrupti, accusantium quibusdam ea.",
+        image: "/Images/Carrusel/Imagen13.jpg",
+        descripcion: "imagen"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Ab aliquid mollitia pariatur natus eaque omnis obcaecati corrupti, accusantium quibusdam ea.",
+        image: "/Images/Carrusel/Image9.jpg",
+        descripcion: "imagen"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Ab aliquid mollitia pariatur natus eaque omnis obcaecati corrupti, accusantium quibusdam ea.",
+        image: "/Images/Carrusel/Image10.jpg",
+        descripcion: "imagen"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Ab aliquid mollitia pariatur natus eaque omnis obcaecati corrupti, accusantium quibusdam ea.",
+        image: "/Images/Carrusel/Imagen11.jpg",
+        descripcion: "imagen"
+    },
+    {
+        title: "Lorem ipsum dolor sit amet.",
+        content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Ab aliquid mollitia pariatur natus eaque omnis obcaecati corrupti, accusantium quibusdam ea.",
+        image: "/Images/Carrusel/Imagen12.jpg",
+        descripcion: "imagen"
+    }
+]
