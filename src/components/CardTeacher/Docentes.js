3ac138723c282409632cc7ab2cd2964c96489d0c
yahir140306 <joseyahirhernandez11@gmail.com>
2024-06-22 21:30:43 -0600

    chore: update teacher names and titles

--- src/components/CardTeacher/Docentes.js
+++ src/components/CardTeacher/Docentes.js
@@ -1,116 +1,110 @@
 export const DOCENTES = [
     {
-        Nombre: "Ana Yeli P.",
-        cargo: "Asistente de Servicios en Plantel",
+        Nombre: "Esther Ibarra Cisneros",
+        cargo: "Profesora",
         image: "/Images/Icons/user.svg",
-        alt: "Asistente"
+        alt: "Maestra"
     },
     {
-        Nombre: "Maria Guadalupe T.",
-        cargo: "Asistente de Servicios y Mantenimiento",
+        Nombre: "Ma. de Lourdes Villegas Rodriguez",
+        cargo: "Profesora",
         image: "/Images/Icons/user.svg",
-        alt: "Asistente"
+        alt: "Maestra"
     },
     {
-        Nombre: "Felipe de S.",
-        cargo: "Maestro",
+        Nombre: "Yucari Concepcion Vivanco Hidalgo",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Maestra"
+    },
+    {
+        Nombre: "Estibaliz Hernandez Salas",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Maestra"
+    },
+    {
+        Nombre: "Manuel Alarcon Rodriguez",
+        cargo: "Profesor",
         image: "/Images/Icons/user.svg",
         alt: "Maestro"
     },
     {
-        Nombre: "Esther I.",
-        cargo: "Maestra",
+        Nombre: "Maria Elena Alarcon Cerecedo",
+        cargo: "Profesora",
         image: "/Images/Icons/user.svg",
         alt: "Maestra"
     },
     {
-        Nombre: "Liliana I.",
-        cargo: "Maestra",
+        Nombre: "Juan Gomez Vega",
+        cargo: "Profesor",
         image: "/Images/Icons/user.svg",
         alt: "Maestro"
     },
     {
-        Nombre: "Ramon P.",
-        cargo: "Maestro",
+        Nombre: "Higina Castillo Lopez",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Maestra"
+    },
+    {
+        Nombre: "Ana Miriam Osorio Balderrama",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Maestra"
+    },
+    {
+        Nombre: "Oliveria Pacheco Marañon",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Maestra"
+    },
+    {
+        Nombre: "Felipe de Jesus Sierra Cortes",
+        cargo: "Profesor",
         image: "/Images/Icons/user.svg",
         alt: "Maestro"
     },
     {
-        Nombre: "Olga Consuelo H.",
-        cargo: "Maestra",
+        Nombre: "Liliana Ibarra Garcia",
+        cargo: "Profesora",
         image: "/Images/Icons/user.svg",
         alt: "Maestra"
     },
     {
-        Nombre: "Bernardiono Fredi B.",
-        cargo: "Maestro",
+        Nombre: "Magali Nochebuena Maya",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Maestra"
+    },
+    {
+        Nombre: "Olga Consuelo Hernandez Hernandez",
+        cargo: "Profesora",
+        image: "/Images/Icons/user.svg",
+        alt: "Profesora"
+    },
+    {
+        Nombre: "Julian Aguilar Morales",
+        cargo: "Profesor",
+        image: "/Images/Icons/user.svg",
+        alt: "Profesor"
+    },
+    {
+        Nombre: "Isaac Eliel Hernandez Lopez",
+        cargo: "Profesor",
         image: "/Images/Icons/user.svg",
         alt: "Maestro"
     },
     {
-        Nombre: "Oralia R.",
-        cargo: "Maestra",
+        Nombre: "Margarita Soveida Rodriguez Castillo",
+        cargo: "Profesor",
         image: "/Images/Icons/user.svg",
         alt: "Maestra"
     },
     {
-        Nombre: "Higinia C.",
-        cargo: "Maestra",
+        Nombre: "Oralia Rodriguez Morales",
+        cargo: "Profesora",
         image: "/Images/Icons/user.svg",
         alt: "Maestra"
-    },
-    {
-        Nombre: "Yucari Concepcion V.",
-        cargo: "Maestra",
-        image: "/Images/Icons/user.svg",
-        alt: "Maestra"
-    },
-    {
-        Nombre: "Magali N.",
-        cargo: "Maestra",
-        image: "/Images/Icons/user.svg",
-        alt: "Maestra"
-    },
-    {
-        Nombre: "Luis Alberto C.",
-        cargo: "Maestro",
-        image: "/Images/Icons/user.svg",
-        alt: "Maestro"
-    },
-    {
-        Nombre: "",
-        cargo: "",
-        image: "",
-        alt: ""
-    },
-    {
-        Nombre: "",
-        cargo: "",
-        image: "",
-        alt: ""
-    },
-    {
-        Nombre: "",
-        cargo: "",
-        image: "",
-        alt: ""
-    },
-    {
-        Nombre: "",
-        cargo: "",
-        image: "",
-        alt: ""
-    },
-    {
-        Nombre: "",
-        cargo: "",
-        image: "",
-        alt: ""
-    },
-    {
-        Nombre: "",
-        cargo: "",
-        image: "",
-        alt: ""
     },
 ]
