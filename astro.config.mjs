5b953f7bdef4d10fc1061e1742793d0ccef19977
yahir140306 <joseyahirhernandez11@gmail.com>
2024-06-23 22:51:12 -0600

    feat: Add Astro integrations for Tailwind, Cloudflare, and DB

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), db()],
+  output: "server",
+  adapter: cloudflare({
+    imageService: "cloudflare",
+    wasmModuleImports: true
+  })
 });
