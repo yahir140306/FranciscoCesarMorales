954c78afd6d6c93d35e34c646e07764d0e1df8ac
yahir140306 <joseyahirhernandez11@gmail.com>
2024-06-23 22:51:55 -0600

    Update middleware to handle session and user data

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
+/// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
+// src/env.d.ts
+declare namespace App {
+  interface Locals {
+    session: import("lucia").Session | null;
+    user: import("lucia").User | null;
+  }
+}
