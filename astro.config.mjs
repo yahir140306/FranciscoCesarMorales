30152e43aaea44a815e9ff8665d43450add2b3db
yahir140306 <joseyahirhernandez11@gmail.com>
2024-06-30 17:24:20 -0600

    chore: Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 // import cloudflare from "@astrojs/cloudflare";
-import db from "@astrojs/db";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    optimizeDeps: {
+      exclude: ["oslo"]
+    }
+  }
 });
